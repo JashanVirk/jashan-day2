644de1d72756e878f6f8f63922a300dd782cb4c1
JashanVirk <35943321+JashanVirk@users.noreply.github.com>
2018-01-30 20:45:30 -0500

    Add files via upload
    jashan d2

--- Contents.swift
+++ Contents.swift
@@ -2,32 +2,76 @@
 
 import UIKit
 
-var  letters = Set<Character>()
-letters.insert("A")
-letters.insert("B")
-print("Count \(letters.count)")
+var str = "Hello, playground"
+var a = [1,2,3,4,5]
 
-letters.insert("A")
-print("Count \(letters.count)")
+print(a[0])
 
+a.append(6)
 
-var str = "Hello World😜"
-var a = "\u{24}"
-var b = "\u{1F496}"
-var c = "\u{65}"
-print(a,b,c)
+print(a[5])
 
-let longString = """
-abfjsfsk bfsb
-sdfsfsbfsdbkfdjs
-sjfksdfbsd
-""";
+var b = [10,20,30]
 
-print(longstring)
+a = a + b
 
-print("\"HELLO\",\JASHAN")
-var s = "Welcome to Canada"
+for i in a
+{
+    print(i)
+}
+var c:[Int]!
 
+print(a.count)
 
+//c?.append(8)
+
+print(c?.count ?? 0)
+ print("---NEW---")
+for i in a[2...5]
+{
+    print(i)
+    
+}
+for i in a[5...]
+{
+    print(i)
+}
+print("---NEW---")
+for i in a[3..<6]
+{
+    print(i)
+}
+ var d = [2...5]
+print("---Special NEW---")
+    for i in d
+{
+    print(i)
+}
+var e = a[2...5]
+
+print("---NEW---")
+for i in e
+{
+    print(i)
+}
+ print("---ONE---")
+print(e[2])
+print("---TWO---")
+print(e[2])
+
+e[2] = 90000
+
+print("---THREE---")
+print(e[2])
+print("---FOUR---")
+print(e[2])
+
+print("Size of e \(e.count)")
+
+var threeDoubles = Array(repeating: 0.0, count: 3)
+ for (k,v) in a.enumerated()
+{
+    print("Index : \(k)-->\(v)")
+}
 
 
