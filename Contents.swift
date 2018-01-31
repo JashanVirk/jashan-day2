946ab494f9b0085e53ea9c22b7c7c98ae0325cd9
JashanVirk <35943321+JashanVirk@users.noreply.github.com>
2018-01-30 20:43:18 -0500

    Add files via upload
    jashan day2

---
+++ Contents.swift
@@ -0,0 +1,33 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var  letters = Set<Character>()
+letters.insert("A")
+letters.insert("B")
+print("Count \(letters.count)")
+
+letters.insert("A")
+print("Count \(letters.count)")
+
+
+var str = "Hello World😜"
+var a = "\u{24}"
+var b = "\u{1F496}"
+var c = "\u{65}"
+print(a,b,c)
+
+let longString = """
+abfjsfsk bfsb
+sdfsfsbfsdbkfdjs
+sjfksdfbsd
+""";
+
+print(longstring)
+
+print("\"HELLO\",\JASHAN")
+var s = "Welcome to Canada"
+
+
+
+
